bc9cfc207432c5154ce62177f7d0b947d6cbea27
Gosuto Inzasheru <gosuto@inzasheru.email>
2023-06-15 19:46:23 -0400

    chore: add required fields for market entity

--- subgraphs/ebtc/src/entities/market.ts
+++ subgraphs/ebtc/src/entities/market.ts
@@ -17,7 +17,7 @@ import {
   updateProtocolBorrowBalance,
   updateProtocolUSDLocked,
 } from "./protocol";
-import { getETHToken, getCurrentETHPrice } from "./token";
+import { getETHToken, getCurrentETHPrice, getEBTCToken } from "./token";
 import {
   Market,
   MarketDailySnapshot,
@@ -31,36 +31,93 @@ export function getOrCreateMarket(): Market {
     market.protocol = getOrCreateEbtcProtocol().id;
     market.name = "eBTC";
     market.isActive = true;
-    market.canUseAsCollateral = true;
     market.canBorrowFrom = true;
+    market.canUseAsCollateral = true;
     market.maximumLTV = MAXIMUM_LTV;
     market.liquidationThreshold = MAXIMUM_LTV;
     market.liquidationPenalty = LIQUIDATION_FEE_PERCENT;
-    market.inputToken = getETHToken().id;
-    market.cumulativeSupplySideRevenueUSD = BIGDECIMAL_ZERO;
-    market.cumulativeProtocolSideRevenueUSD = BIGDECIMAL_ZERO;
-    market.cumulativeTotalRevenueUSD = BIGDECIMAL_ZERO;
+    market.canIsolate = false;
     market.createdTimestamp = ACTIVE_POOL_CREATED_TIMESTAMP;
     market.createdBlockNumber = ACTIVE_POOL_CREATED_BLOCK;
+    // market.oracle: Oracle // " Details about the price oracle used to get this token's price "
+    // market.relation: Bytes // " A unique identifier that can relate multiple markets. e.g. a common address that is the same for each related market. This is useful for markets with multiple input tokens "
 
+    // ##### Incentives #####
+    // market.rewardTokens: [RewardToken!] // " Additional tokens that are given as reward for position in a protocol, usually in liquidity mining programs. e.g. SUSHI in the Onsen program, MATIC for Aave Polygon "
+    // market.rewardTokenEmissionsAmount: [BigInt!] // " Per-block reward token emission as of the current block normalized to a day, in token's native amount. This should be ideally calculated as the theoretical rate instead of the realized amount. "
+    // market.rewardTokenEmissionsUSD: [BigDecimal!] // " Per-block reward token emission as of the current block normalized to a day, in USD value. This should be ideally calculated as the theoretical rate instead of the realized amount. "
+    // market.stakedOutputTokenAmount: BigInt // " Total supply of output tokens that are staked. Used to calculate reward APY. "
+
+    // ##### Quantitative Data #####
+    market.inputToken = getETHToken().id;
+    market.inputTokenBalance = BIGINT_ZERO;
+    market.inputTokenPriceUSD = BIGDECIMAL_ZERO;
+    // market.outputToken: Token // " Tokens that are minted to track ownership of position in protocol (e.g. aToken, cToken). Leave as null if doesn't exist (should be alphabetized) "
+    market.outputTokenSupply = BIGINT_ZERO;
+    market.outputTokenPriceUSD = BIGDECIMAL_ZERO;
+    // market.exchangeRate: BigDecimal // " Amount of input token per full share of output token. Only applies when the output token exists (note this is a ratio and not a percentage value, i.e. 1.05 instead of 105%) "
+    // market.rates: [InterestRate!] // " All interest rates for this input token. Should be in APR format "
+    // market.reserves: BigDecimal // " Total amount of reserves (in USD) "
+    // market.reserveFactor: BigDecimal // " The amount of revenue that is converted to reserves at the current time. 20% reserve factor should be in format 0.20 "
+    market.borrowedToken = getEBTCToken().id;
+    // market.variableBorrowedTokenBalance: BigInt // " Amount of input tokens borrowed in this market using variable interest rates (in native terms) "
+    // market.stableBorrowedTokenBalance: BigInt // " Amount of input tokens borrowed in this market using stable interest rates (in native terms) "
+    // market.indexLastUpdatedTimestamp: BigInt // " Last updated timestamp of supply/borrow index. "
+    // market.supplyIndex: BigInt // " Index used by the protocol to calculate interest generated on the supply token (ie, liquidityIndex in Aave)"
+    // market.supplyCap: BigInt // " Allowed limit to how much of the underlying asset can be supplied to this market. "
+    // market.borrowIndex: BigInt // " Index used by the protocol to calculate the interest paid on the borrowed token (ie, variableBorrowIndex in Aave))"
+    // market.borrowCap: BigInt // " Allowed limit for how much of the underlying asset can be borrowed from this market. "
     market.totalValueLockedUSD = BIGDECIMAL_ZERO;
     market.cumulativeSupplySideRevenueUSD = BIGDECIMAL_ZERO;
     market.cumulativeProtocolSideRevenueUSD = BIGDECIMAL_ZERO;
     market.cumulativeTotalRevenueUSD = BIGDECIMAL_ZERO;
+    // market.revenueDetail: RevenueDetail // " Details of revenue sources and amounts "
     market.totalDepositBalanceUSD = BIGDECIMAL_ZERO;
     market.cumulativeDepositUSD = BIGDECIMAL_ZERO;
     market.totalBorrowBalanceUSD = BIGDECIMAL_ZERO;
     market.cumulativeBorrowUSD = BIGDECIMAL_ZERO;
     market.cumulativeLiquidateUSD = BIGDECIMAL_ZERO;
-    market.inputTokenBalance = BIGINT_ZERO;
-    market.inputTokenPriceUSD = BIGDECIMAL_ZERO;
-    market.outputTokenSupply = BIGINT_ZERO;
-    market.outputTokenPriceUSD = BIGDECIMAL_ZERO;
+    market.cumulativeTransferUSD = BIGDECIMAL_ZERO;
+    market.cumulativeFlashloanUSD = BIGDECIMAL_ZERO;
+    market.transactionCount = INT_ZERO;
+    market.depositCount = INT_ZERO;
+    market.withdrawCount = INT_ZERO;
+    market.borrowCount = INT_ZERO;
+    market.repayCount = INT_ZERO;
+    market.liquidationCount = INT_ZERO;
+    market.transferCount = INT_ZERO;
+    market.flashloanCount = INT_ZERO;
+
+    // ##### Usage Data #####
+    market.cumulativeUniqueUsers = INT_ZERO;
+    market.cumulativeUniqueDepositors = INT_ZERO;
+    market.cumulativeUniqueBorrowers = INT_ZERO;
+    market.cumulativeUniqueLiquidators = INT_ZERO;
+    market.cumulativeUniqueLiquidatees = INT_ZERO;
+    market.cumulativeUniqueTransferrers = INT_ZERO;
+    market.cumulativeUniqueFlashloaners = INT_ZERO;
+
+    // ##### Account/Position Data #####
+    // market.positions: [Position!]! @derivedFrom(field: "market") // " All positions in this market "
     market.positionCount = INT_ZERO;
     market.openPositionCount = INT_ZERO;
     market.closedPositionCount = INT_ZERO;
     market.lendingPositionCount = INT_ZERO;
     market.borrowingPositionCount = INT_ZERO;
+
+    // ##### Snapshots #####
+    // market.dailySnapshots: [MarketDailySnapshot!]! @derivedFrom(field: "market") // " Market daily snapshots "
+    // market.hourlySnapshots: [MarketHourlySnapshot!]! @derivedFrom(field: "market") // " Market hourly snapshots "
+
+    // ##### Events #####
+    // market.deposits: [Deposit!]! @derivedFrom(field: "market") // " All deposits made to this market "
+    // market.withdraws: [Withdraw!]! @derivedFrom(field: "market") // " All withdrawals made from this market "
+    // market.borrows: [Borrow!]! @derivedFrom(field: "market") // " All borrows from this market "
+    // market.repays: [Repay!]! @derivedFrom(field: "market") // " All repayments to this market "
+    // market.liquidates: [Liquidate!]! @derivedFrom(field: "market") // " All liquidations made to this market "
+    // market.transfers: [Transfer!]! @derivedFrom(field: "market") // " All transfers made in this market "
+    // market.flashloans: [Flashloan!]! @derivedFrom(field: "market") // " All flashloans made in this market"
+
     market.save();
   }
   return market;
